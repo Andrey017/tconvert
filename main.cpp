0c5c14657ae10856ba2798f9e23a8170ec1bd626
Andrey <32015678+Andrey017@users.noreply.github.com>
2017-10-07 20:21:59 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//#include <QCoreApplication>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+    float t=atof(argv[1]);
+    int input, x, y;
+    input = (int)(argv[2][0]);
+        switch ( input ) {
+            case 'c':
+                x=t+273;
+                y=9*t/5+32;
+                printf("%dK", x);
+                printf("\n%dF", y);
+            break;
+            case 'k':
+                x=t-273;
+                y=9*(t-273)/5+32;
+                printf("%dC", x);
+                printf("\n%dF", y);
+            break;
+            case 'f':
+                x=5*(t-32)/9;
+                y=5*(t-32)/9+273;
+                printf("%dC", x);
+                printf("\n%dK", y);
+            break;
+            default:
+                printf("-1");
+        }
+    //return a.exec();
+    return 0;
+}
